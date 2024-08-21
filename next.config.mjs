6f893e3adc698d41e0902bae0ebfe5f892901028
unknown <kamil.kamil.1995@wp.pl>
2024-08-21 13:02:41 +0200

    Update Next.js config and add environment variables for Vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,13 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    CONTENTFUL_PREVIEW_ACCESS_TOKEN:
+      process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+    ADD_POST_TOKEN: process.env.ADD_POST_TOKEN,
+  },
 };
 
 export default nextConfig;
